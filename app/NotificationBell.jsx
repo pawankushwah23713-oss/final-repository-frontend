948e71151af6df5b8d80cc475912edeb6a8bb4e5
pawankushwah23713-oss <pawankushwah23713@gmail.com>
2026-05-16 00:59:26 +0530

    Update NotificationBell.jsx

--- app/NotificationBell.jsx
+++ app/NotificationBell.jsx
@@ -17,7 +17,7 @@ export default function NotificationBell() {
 
     const fetchNotifications = () => {
       fetch(
-        `https://final-repository-production.up.railway.app/api/products/pending?username=${encodeURIComponent(
+        `https://final-repository-3.onrender.com/api/products/pending?username=${encodeURIComponent(
           userEmail
         )}`
       )
@@ -47,7 +47,7 @@ export default function NotificationBell() {
     window.addEventListener("online", fetchNotifications);
 
     const socket = new SockJS(
-      `https://shopingbackend-production.up.railway.app/ws?username=${encodeURIComponent(
+      `https://final-repository-3.onrender.com/ws?username=${encodeURIComponent(
         userEmail
       )}`
     );
