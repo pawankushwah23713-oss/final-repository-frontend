4cb36e53933c03840f098af8abf2907c4e3bde25
pawankushwah23713-oss <pawankushwah23713@gmail.com>
2026-05-16 00:38:58 +0530

    Change API endpoints for product operations
    Updated API endpoint URLs for product profile and deletion.

--- app/Profile/page.js
+++ app/Profile/page.js
@@ -10,7 +10,7 @@ export default function ProductsPage() {
   const router = useRouter();
 
   useEffect(() => {
-    fetch("https://final-repository-production.up.railway.app/api/products/profile", {
+    fetch("https://final-repository-3.onrender.com/api/products/profile", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -23,7 +23,7 @@ export default function ProductsPage() {
   }, []);
 
   const handleDelete = (productId) => {
-    fetch(`https://final-repository-production.up.railway.app/api/products/${productId}`, {
+    fetch(`https://final-repository-3.onrender.com/api/products/${productId}`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
